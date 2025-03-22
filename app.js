a9794256e222d3a8a01e265443854c357b5d59da
insta <anmolpreetsingh510@gmail.com>
2025-03-23 04:26:38 +0530

    Listing Model

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const PORT = 8080;
+const Listing = require("./models/listing.js");
 
-app.get("/", (req, res) => {
-  res.send("Hi i am root");
-});
 
 const MNGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +19,28 @@ async function main() {
   await mongoose.connect(MNGO_URL);
 }
 
+app.get("/", (req, res) => {
+  res.send("Hi i am root");
+});
+
+app.get("/testListing", async (req ,res )=>{
+  // res.send("working Good")
+  
+  let sampleListing  = new Listing( {
+    title : "my New Villa",
+    description : "by the beach",
+    price : 1200,
+    location : "calangute , goa ",
+    country : " India"
+  });
+
+  await sampleListing.save();
+  console.log("sample");
+  res.send("successfull testing ");
+});
+
+
+
 
 
 
@@ -29,3 +49,11 @@ app.listen(PORT, (error) => {
   if (!error) console.log("Server is successful Running" + PORT);
   else console.log("Error", error);
 });
+
+// Listing.deleteMany({title: "my New Villa"})
+// .then((res)=>{
+//   console.log(res)
+// })
+// .catch((err)=>{
+//   console.log(err)
+// })
---
+++ models/listing.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title : {
+        type :String,
+        required :true,
+    },
+
+    description : String,
+    image: {
+        type : String,
+        default : "https://pixelstalk.net/wp-content/uploads/2016/08/Sunset-Beaches-Backgrounds.jpg",
+        set: (v) => v.trim() === " " ? "https://pixelstalk.net/wp-content/uploads/2016/08/Sunset-Beaches-Backgrounds.jpg" : v ,   // check v is empty OR  note  ( Ex. if check V is empty Or not  / if V is a empty then print link / v is not Empty than print a defalt value)
+    },
+    price : Number,
+    location : String,
+    country : String,
+});
+
+
+
+
+const Listing = mongoose.model("Listing",listingSchema);
+module.exports = Listing;
+
+
+// const DataLinf
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
